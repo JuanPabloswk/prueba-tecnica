92675d46a312dfcc73e2130d47beb70d3cef4338
JuanPabloswk <juampa2018801mail.com>
2025-08-05 15:29:58 -0500

    Gateway issues resolved

--- inventory-service/src/main/java/com/example/inventoryservice/client/ProductClient.java
+++ inventory-service/src/main/java/com/example/inventoryservice/client/ProductClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
-@FeignClient(name = "product-service", url = "http://localhost:8081")
+@FeignClient(name = "product-service", url = "http://localhost:8080")
 public interface ProductClient {
 
     @GetMapping("/api/products/{id}")
